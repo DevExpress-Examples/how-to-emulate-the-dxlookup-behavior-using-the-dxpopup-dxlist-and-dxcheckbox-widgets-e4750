c76e10c4948215a6bca0e296982a831759419865
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/DxSample/app.js
@@ -0,0 +1,11 @@
+﻿window.DxSample = window.DxSample || {};
+
+$(function() {
+    app = new DevExpress.framework.html.HtmlApplication({
+        ns: DxSample,
+        viewPortNode: document.getElementById("viewPort"),
+        defaultLayout: DxSample.config.defaultLayout,
+        navigation: DxSample.config.navigation
+    });
+    app.router.register(":view/:id", { view: "Index", id: undefined });
+});
