0c238ebb32099898a6c757963b96594afcef1981
DevExpressExampleBot <fakeEmail>
2020-12-08 19:40:49 +0300

    Source auto update [skip ci]

---
+++ VB/JS/DxSample/data/operating-system-view-model.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    DxSample.OperatingSystemViewModel = function (data) {
+        var self = this;
+
+        self.name = ko.observable();
+        self.selected = ko.observable(false);
+
+        if (data)
+            self.fromJS(data);
+    };
+
+    $.extend(DxSample.OperatingSystemViewModel.prototype, {
+        toJS: function () {
+            return { name: this.name() };
+        },
+
+        fromJS: function (data) {
+            if (data)
+                this.name(data.name);
+        }
+    });
+})();
---
+++ VB/Readme.md
@@ -0,0 +1,12 @@
+<!-- default file list -->
+*Files to look at*:
+
+<!-- default file list end -->
+# How to emulate the dxLookup behavior using the dxPopup, dxList and dxCheckBox widgets
+
+
+<p>At present the dxLookup does not support templates. So, the only way to customize it (e.g., implement multiple selection or show several columns) is to emulate its behavior using other widgets.</p>
+
+<br/>
+
+
